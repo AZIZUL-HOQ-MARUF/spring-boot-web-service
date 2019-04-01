fe190edb2bb9e2b3b79707fe79671d0ee8e06c24
AZIZUL HOQ <42655898+AZIZUL-HOQ-MARUF@users.noreply.github.com>
2019-04-02 00:38:25 +0600

    name changed

--- src/main/java/com/maruf/restwebservices/HelloWorldController.java
+++ src/main/java/com/maruf/restwebservices/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.maruf.restwebservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
-public class HelloWorld {
+public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
